3ae1b6d8bb72de9953fa311dc0fbd0913be67727
HumphreyJ <joshhumphrey1@gmail.com>
2020-01-23 18:40:16 -0600

    Auth completed and data renders from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from "./components/Login";
+import { BrowserRouter as Router, Route,Link,Redirect } from "react-router-dom";
+import BubblePage from './components/BubblePage';
+import BestLogin from "./components/BestLogin";
 import "./styles.scss";
 
 function App() {
+
+  const PrivateRoute = ({component: BubblePage, ...rest}) => (
+    <Route {...rest} render={(props) => 
+    localStorage.getItem('token') ? (
+      <BubblePage {...props} />
+    ) : (
+      <Redirect to='/login' />
+    )
+    } />
+  )
+
+
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Route exact path="/" component={BestLogin} />
+        <PrivateRoute path='/bubblesPage' component={BubblePage} />
       </div>
     </Router>
   );
